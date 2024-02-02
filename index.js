fb50ae7528564c262868dfe68d92a487dfada2d0
xodnd0507 <24457545yong@gmail.com>
2024-02-02 15:54:23 +0900

    Test : content and logo update

---
+++ index.js
@@ -0,0 +1,12 @@
+var count = 0;
+
+$('.change').on('click', function(){
+    count++;
+    if(count % 2 == 1){
+        $('.change').text("Lightmode");
+        $('.mode').addClass('dark');
+    } else {
+        $('.change').text("Darkmode");
+        $('.mode').removeClass('dark');
+    }
+})
