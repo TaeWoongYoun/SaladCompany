da53ba74a68297d853a304be226980671ac751e4
xodnd0507 <24457545yong@gmail.com>
2024-02-25 00:16:57 +0900

    feat : Add product page

---
+++ index.js
@@ -0,0 +1,10 @@
+for(let i = 0; i < $('.tab-button').length; i++){
+
+    $('.tab-button').eq(i).on('click', function(){
+        $('.tab-button').removeClass('orange');
+        $('.tab-button').eq(i).addClass('orange');
+        $('.tab-content').removeClass('show');
+        $('.tab-content').eq(i).addClass('show');
+    })
+    }
+    
